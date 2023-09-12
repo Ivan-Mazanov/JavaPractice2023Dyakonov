feabf34f59a0190453f32dae1a1f006f9dbedff7
dyakonov.dd.kst <dyakonov.dd@mmtr.ru>
2023-09-12 11:08:35 +0300

    add dict service

---
+++ src/dict/service/DictService.java
@@ -0,0 +1,68 @@
+package dict.service;
+
+import dict.dataProvider.CsvFileIO;
+import dict.dataProvider.FileIO;
+import dict.dataProvider.FileType;
+import dict.dataSource.Dictionary;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static dict.service.Converter.getStringFromHashMap;
+
+//Сервис для работы со словарем. К нему обращается UI
+public class DictService {
+    private FileIO fIleIO;
+    private DictContainer dictContainer = new DictContainer();
+
+
+    public DictService(FileType fileType) {
+        switch (fileType) {
+            case CSV -> fIleIO = new CsvFileIO();
+        }
+    }
+
+    public boolean initializeDict(String dictName, String filePath, Predicate<String> validationFunc) {
+        HashMap<String, String> hashMap = fIleIO.getDataFromFile(filePath);
+
+        Dictionary dictionary = new Dictionary(dictName, filePath, hashMap, validationFunc);
+
+        return dictContainer.addDict(dictionary);
+    }
+
+    public List<String> getNames() {
+        List<String> nameList = new ArrayList<>();
+
+        for (Dictionary dictionary : dictContainer.getCopyOfContent()) {
+
+            nameList.add(dictionary.getDictionaryName());
+        }
+        return nameList;
+    }
+
+    public boolean addNewPair(String dictName, String key, String value) {
+        if (key == null)
+            return false;
+        return dictContainer.GetDict(dictName).addPair(key, value);
+    }
+
+    public String getDictString(String dictName) {
+        var dict = dictContainer.GetDict(dictName).getContent();
+
+        return getStringFromHashMap(dict, ":", System.lineSeparator());
+    }
+
+    public String findByKey(String dictName, String key) {
+        return dictContainer.GetDict(dictName).findByKey(key);
+    }
+
+    public boolean deleteByKey(String dictName, String key) {
+        return dictContainer.GetDict(dictName).deleteByKey(key);
+    }
+
+    public void saveData(String dictName) {
+        fIleIO.loadDataInFile(dictContainer.GetDict(dictName));
+    }
+}
